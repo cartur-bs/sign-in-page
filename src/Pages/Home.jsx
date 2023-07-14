0aadc5d13748321f4bd4366690107ebc78794300
Carlos Artur <cartur758@gmail.com>
2023-07-14 20:47:17 -0300

    correção media query pg2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './Home.css'
 import iconList from '../assets/icon-list.svg';
-import illustrationDesktop from '../assets/illustration-sign-up-desktop.svg';
 import successImg from '../assets/icon-success.svg';
 
 
@@ -21,36 +20,31 @@ const Home = () => {
         setInputText('');
         setemptyInputTxt('');
     }
-
-
     return (
         <div>
-
             {
-
                 isVisible ? (
                     <div className='homePage'>
                         <div className='mainTxt'>
                             <h1>Mantenha-se atualizado!</h1>
-                            <p>Join 60.000+ product managers receiving monthly updates on:</p>
-
+                            <p> Se junte a mais de 60.000 gerentes de produção recebendo atualizações em: </p>
                             <ul>
                                 <li>
                                     <img src={iconList} alt="checked icon" className='listICon' />
-                                    Product discovery and building what matters
+                                    Descoberta de produtos e construção do que importa
                                 </li>
                                 <li>
                                     <img src={iconList} alt="checked icon" className='listICon' />
-                                    Measuring to ensure updates are a sucecess
+                                    Mantendo constância nas atualizações é a chave do sucesso
                                 </li>
                                 <li>
                                     <img src={iconList} alt="checked icon" className='listICon' />
-                                    And much more!
+                                    E muito mais!
                                 </li>
                             </ul>
                             <form>
                                 <label>
-                                    <span>Email adress</span>
+                                    <span>Email</span>
                                     <span >{emptyInputTxt}</span>
                                     <input type='email' placeholder='email@company.com' className='inputText' required onChange={(e) => setInputText(e.target.value)} />
                                     <button onClick={handleEmail} type="submit"> Inscreva-se</button>
@@ -70,7 +64,7 @@ const Home = () => {
                         <div>
                             <img src={successImg} alt="" />
                             <h1>Obrigado por se inscrever!</h1>
-                            <p>Um email de confirmação foi enviado para {inputText}. Por favor, abra e clique no botão no corpo do email para confirmar sua inscrição.</p>
+                            <p>Um email de confirmação foi enviado para <span className="enderecoEmail"> {inputText} </span>. Por favor, abra e clique no botão no corpo do email para confirmar sua inscrição.</p>
                             <button onClick={finishSubscritpionProccess} className='dismissBtn'> Dispensar mensagem </button>
                         </div>
                     </div>)}
